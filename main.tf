d8cc352e34eba983fa09c2c33125e84e65c1ff66
AbelRapha <abelrapha@gmail.com>
2023-04-11 02:10:12 -0300

    terraform with ansible

--- main.tf
+++ main.tf
@@ -18,8 +18,13 @@ resource "aws_instance" "server_app" {
     ami = "ami-0a695f0d95cefc163"
     instance_type = "t2.micro"
     key_name = "server-app"
-
+    # user_data = <<-EOF
+    #                #!/bin/bash
+    #                cd /home/ubuntu
+    #                echo "<h1>Feito com Terraform</h1>" > index.html
+    #                nohup busybox httpd -f -p 8080 &
+    #                EOF
     tags = {
-      "Name" = "ExampleAppServerInstance"
+      "Name" = "ServidorFeitocomTerraform"
     }
 }
